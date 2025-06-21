25e2a7b386729b100bd0a5ca1d9de12051d2cef1
Hady Hassan <99578132+hady444@users.noreply.github.com>
2025-06-21 19:56:38 +0200

    Delete Agents_online/src/agents_online/application/evaluation directory

--- Agents_online/src/agents_online/application/evaluation/__init__.py
+++
@@ -1,5 +0,0 @@
-from .evaluate import evaluate_agent
-from .summary_density_heuristic import SummaryDensityHeuristic
-from .summary_density_judge import SummaryDensityJudge
-
-__all__ = ["evaluate_agent", "SummaryDensityHeuristic", "SummaryDensityJudge"]
